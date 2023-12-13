dfded0ed6f534bafec179324bfc6668a6dea919b
Sally-N-Ngure <nguresallynjoki@gmail.com>
2023-12-14 00:49:01 +0300

    Merged changes from remote repository

--- src/main/java/com/example/demo/CarService.java
+++ src/main/java/com/example/demo/CarService.java
@@ -1,22 +1,37 @@
 package com.example.demo;
-
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 public class CarService {
     public CarRepository carRepository;
-
     //add new car
     public Car addCar(Car car){
         return carRepository.save(car);
     }
-
     // get cars
     public List<Car> getCars(){
         return carRepository.findAll();
     }
 
+    // GET single entity by ID
+    public Car getSingleResident(Long id ){
+        return carRepository.findById(id).orElseThrow();
+    }
+
+    // search by first Name
+//    public List<Car> getByFirstName(String name){
+//        return carRepository.findByFirstName(name);
+//    }
+
+    // get all Resident
+    public List<Car> getAllResidents(){
+        return carRepository.findAll();
+    }
+
+    // delete resident
+    public String deleteResident(Long id){
+        carRepository.deleteById(id);
+        return "User id: " + id + " deleted";
+    }
 
 }
